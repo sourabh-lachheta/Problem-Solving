0572ca0ae08e2b69fdb49d7b1668b629f9273f95
sourabh <sourabhlachheta@gmail.com>
2025-10-11 22:15:20 +0530

    analayis the question

--- assignments/src/com/LeetcodeProblem/MaxPopulation.java
+++ assignments/src/com/LeetcodeProblem/MaxPopulation.java
@@ -2,6 +2,10 @@ package com.LeetcodeProblem;
 
 public class MaxPopulation {
     public static void main(String[] args){
+       int[][] year = {{1940, 1950},{1950, 1960}};
+       getresult(year);
+    }
+    static int getresult(int[][] year){
 
     }
 }
