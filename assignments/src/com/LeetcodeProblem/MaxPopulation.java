784dc57624c6618e833991349bae2c0dcdb6d833
sourabh <sourabhlachheta@gmail.com>
2025-10-15 19:34:46 +0530

    max population solving

--- assignments/src/com/LeetcodeProblem/MaxPopulation.java
+++ assignments/src/com/LeetcodeProblem/MaxPopulation.java
@@ -7,18 +7,7 @@ public class MaxPopulation {
     }
     static int getresult(int[][] year){
 
-        int start = 0, sum = 0;
-        int end = nums.length - 1;
-        while(start <= end){
-            sum = sum + nums[start][start] + nums[end][start] + nums[end][end] + nums[start][end] ;
-            if(start == end){
-                sum = sum - (nums[end][end] + nums[end][start] + nums[start][end]);
-            }
 
-            start++;
-            end--;
-        }
-        return sum;
 
     }
 }
