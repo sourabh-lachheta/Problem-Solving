fc69d333749c672b8fc4abb9af4b6e781adfb0a0
sourabh <sourabhlachheta@gmail.com>
2025-10-17 21:33:07 +0530

    analyze rotation matrix

--- assignments/src/com/LeetcodeProblem/RotationMatrix.java
+++ assignments/src/com/LeetcodeProblem/RotationMatrix.java
@@ -2,6 +2,9 @@ package com.LeetcodeProblem;
 
 public class RotationMatrix {
     public static void main(String[] args){
-        
+        getresult();
+    }
+    static int[][] getresult(){
+
     }
 }
