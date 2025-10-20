024db0e8a9762596086c1d8838328b1ba0a8111f
sourabh <sourabhlachheta@gmail.com>
2025-10-20 22:22:38 +0530

    solving

--- assignments/src/com/LeetcodeProblem/RotationMatrix.java
+++ assignments/src/com/LeetcodeProblem/RotationMatrix.java
@@ -2,9 +2,11 @@ package com.LeetcodeProblem;
 
 public class RotationMatrix {
     public static void main(String[] args){
-        getresult();
+        int[] num = {1,2,3,4};
+        getresult(num);
     }
-    static int[][] getresult(){
+    static int[][] getresult(int[] num){
+        int[] ans = new int[num.length];
 
     }
 }
