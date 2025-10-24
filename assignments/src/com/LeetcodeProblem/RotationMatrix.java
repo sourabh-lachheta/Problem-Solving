aa5ee9a3dabeb42c177a856998650ef9ae68871c
sourabh <sourabhlachheta@gmail.com>
2025-10-24 22:44:03 +0530

    solving matrix

--- assignments/src/com/LeetcodeProblem/RotationMatrix.java
+++ assignments/src/com/LeetcodeProblem/RotationMatrix.java
@@ -7,6 +7,19 @@ public class RotationMatrix {
     }
     static int[][] getresult(int[] num){
         int[] ans = new int[num.length];
+        int start = 0, sum = 0;
+        int end = nums.length - 1;
+        while(start <= end){
+            sum = sum + nums[start][start] + nums[end][start] + nums[end][end] + nums[start][end] ;
+            if(start == end){
+                sum = sum - (nums[end][end] + nums[end][start] + nums[start][end]);
+            }
+
+            start++;
+            end--;
+        }
+        return sum;
+
 
     }
 }
